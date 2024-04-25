26df9441e70dcaf2faab2f0d5f4a0635f7f9e3e9
prersn <prerananaik11@gmail.com>
2024-04-25 23:04:57 +0530

    Initial import of existing repository

---
+++ pub_cn.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as mqtt
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+import os
+import time
+
+def encrypt(message, key):
+    key = key.ljust(16)[:16]
+    cipher = Cipher(algorithms.AES(key), modes.CFB(b'\x00'*16), backend=default_backend())
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(message) + padder.finalize()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    return ciphertext
+
+mqttBroker = "mqtt.eclipseprojects.io"
+client = mqtt.Client("Temperature_Inside")
+client.connect(mqttBroker)
+
+n = int(input("Enter the number of tasks under this topic: "))
+i = 0
+while i < n:
+    tasks_message1 = input(f"Enter message for task {i + 1}: ")
+    encrypted_data1 = encrypt(tasks_message1.encode("utf-8"), b'your_secret_key')
+    client.publish("cn", encrypted_data1)
+    print(f"Just published encrypted data for task {i + 1} to Topic cn")
+    time.sleep(1)
+    i = i + 1
---
+++ sub1.py
@@ -0,0 +1,52 @@
+import paho.mqtt.client as mqtt
+import ssl
+import time
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+import os
+
+def decrypt(ciphertext, key):
+    key = key.ljust(16)[:16]
+    cipher = Cipher(algorithms.AES(key), modes.CFB(b'\x00'*16), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadder = padding.PKCS7(128).unpadder()
+    unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+    return unpadded_data
+
+def on_message(client, userdata, message):
+    try:
+        secret_key = b'your_secret_key'
+        decrypted_data = decrypt(message.payload, secret_key)
+        print("Received decrypted message on topic '{}': {}".format(message.topic, decrypted_data.decode("utf-8")))
+    except Exception as e:
+        print(f"Error processing message: {str(e)}")
+
+mqttBroker = "mqtt.eclipseprojects.io"
+
+topic = input("Enter the topic to subscribe to: ")
+
+tls_context = ssl.create_default_context()
+tls_context.load_verify_locations(cafile=r"mosquitto.crt")
+
+client = mqtt.Client("Smartphone")
+client.tls_set_context(context=tls_context)
+client.on_message = on_message
+
+try:
+    client.connect(mqttBroker, port=8883)
+except Exception as e:
+    print(f"Error connecting to MQTT broker: {str(e)}")
+    exit()
+
+client.loop_start()
+client.subscribe(topic)
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    pass
+
+client.loop_stop()
